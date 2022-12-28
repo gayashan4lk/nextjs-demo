98bd5122b895cf6445d714a13146afcbe7edb3e0
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-12-28 19:43:05 +0530

    add layout component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Link from 'next/link';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
 	return (
-		<>
+		<Layout>
 			<h1>First Post 1</h1>
 			<Link href='/'>Back to home</Link>
-		</>
+		</Layout>
 	);
 }
